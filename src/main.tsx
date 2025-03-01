d864fd55570f93f852c651a35d0b09a0be29fe5e
ABDEL MONIM MAZGOURA <mazgouraabdalmounim@gmail.com>
2025-03-01 16:16:20 +0000

    Ajout de Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,4 @@ import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import { SpeedInsights } from "@vercel/speed-insights/react"
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <SpeedInsights /> {/* Ajout de Speed Insights ici */}
-    <App />
-  </React.StrictMode>
-)
